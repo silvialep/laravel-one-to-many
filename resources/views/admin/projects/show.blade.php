520023ccd728f2881e1ab43d6780310dc8bd8ca2
silvialep <silvia.lepore_sl@libero.it>
2023-05-23 20:43:20 +0200

    feat: showed project type in show page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 
 <div class="main pt-5">
-  <h1>{{$project->title}}</h1>
+  <div class="d-flex justify-content-between align-items-center">
+    <h1>{{$project->title}}</h1>
+    <span><a href="{{route('admin.types.index')}}">{{$project->type->type_name ?? ''}}</a></span>
+  </div>
   <hr>
   <h4>{{$project->description}}</h4>
   <p>
