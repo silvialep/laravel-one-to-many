a4225b98d527eed3079f2219a56c941f5dbd07b9
silvialep <silvia.lepore_sl@libero.it>
2023-05-23 22:33:31 +0200

    feat: added links to projects in type index

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -1,1 +1,36 @@
-prova
+@extends('layouts/admin')
+
+@section('content')
+
+
+<table class="table mt-3">
+    <thead>
+        <th>Nome</th>
+        <th>Descrizione</th>
+        <th>Articoli</th>
+    </thead>
+    <tbody>
+        @foreach($types as $type)
+        <tr>
+            <td>{{$type->type_name}}</td>
+            <td>{{$type->description}}</td>
+            @if(count($type->projects) > 0)
+            <td>
+                <ul style="padding:0">
+                    @foreach($type->projects as $item)
+                    <li>
+                    <a href="{{route('admin.projects.show', $item)}}">{{$item->title}}</a>
+                    </li>
+                    @endforeach
+
+                </ul>
+            </td>
+            @else
+            <td class="fst-italic text-danger px-3">NN</td>
+            @endif
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
